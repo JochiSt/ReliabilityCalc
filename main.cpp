66857a18d1b1ce7e9389b5a4d508377fd0d97ea8
JochiSt <jochen.steinmann@web.de>
2016-03-31 13:47:18 +0200

    CLEANUP in main and component
    removed some obsolete stuff and old comments

--- main.cpp
+++ main.cpp
@@ -14,16 +14,11 @@ using namespace std;
 
 int main(){
     cout << "Reliability Calculator" << endl;
-    cout << "\tcompiled @ " << __DATE__ << " " << __TIME__ << " using GCC " << __VERSION__ << endl;
-
-    cout << endl;
-    cout << "###############################################################################" << endl;
-    cout << endl;
+    cout << "\tcompiled @ " << __DATE__ << " " << __TIME__ << " using GCC " << __VERSION__ << endl << endl;
 
     schematic* CLKdecoupling = new schematic("Clock Decoupling");
     CLKdecoupling -> addComponent(new resistor("R1", 1*resistor::kOhm, 10*resistor::mW, 100*resistor::mW, resistor::Q_M, resistor::S_RM) );
     CLKdecoupling -> addComponent(new capacitor("C1", 15*capacitor::nF, 200+sqrt(2)*50., 400., 85 , capacitor::Q_MIL, capacitor::S_CQ));
-
 
     cout << "###############################################################################" << endl;
     cout << endl;
