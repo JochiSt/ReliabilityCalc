9650d9b65f1bbcce9af695f7f535d7fb87ce0a19
Thomas Mueller <t.muller@cern.ch>
2016-08-25 17:37:51 +0200

    screening_strength_plots.py: more options

--- python/screening_strength_plots.py
+++ python/screening_strength_plots.py
@@ -18,8 +18,14 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Make screening strength plots.",
 	                                 parents=[logger.loggingParser])
 
-	parser.add_argument("-s", "--screening-strengths", nargs="+", default=[0.99, 0.995],
+	parser.add_argument("--screening-strengths", nargs="+", type=float, default=[0.99, 0.995],
 	                    help="Screening strength values.")
+	parser.add_argument("--temp-ranges", nargs="+", type=float, default=[50.0, 100.0, 150.0],
+	                    help="Temperature range values.")
+	parser.add_argument("--temp-rates", nargs="+", type=float, default=[3.0, 4.0, 5.0],
+	                    help="Temperature rate of change values.")
+	parser.add_argument("--test-times", nargs="+", type=float, default=[24.0, 48.0],
+	                    help="Test time values.")
 	
 	parser.add_argument("--temp-range-bins", default="100,50,150",
 	                    help="Binning for axes showing the temperature range.")
@@ -27,7 +33,7 @@ if __name__ == "__main__":
 	                    help="Binning for axes showing the temperature rate of change.")
 	parser.add_argument("--n-cycles-bins", default="100,0,40",
 	                    help="Binning for axes showing the number of cycles.")
-	parser.add_argument("--time-test-bins", default="100,0,48",
+	parser.add_argument("--test-time-bins", default="100,0,48",
 	                    help="Binning for axes showing the test time.")
 	
 	parser.add_argument("-a", "--args", default=" --plot-modules PlotRoot",
@@ -43,18 +49,18 @@ if __name__ == "__main__":
 	logger.initLogger(args)
 	
 	screening_strength_strings = [str(s) for s in args.screening_strengths]
+	temp_range_strings = [str(s) for s in args.temp_ranges]
+	temp_rate_strings = [str(s) for s in args.temp_rates]
+	test_time_strings = [str(s) for s in args.test_times]
 	
 	plots = [
-		"thermal_cycling_screening_strength",
 	]
 	plot_configs = {}
 	for plot in plots:
 		plot_configs[plot] = jsonTools.JsonDict(os.path.join("plots", plot+".json"))
 		plot_configs[plot]["output_dir"] = args.output_dir
 	
-	plot_configs["thermal_cycling_screening_strength"]["x_bins"] = args.n_cycles_bins
-	plot_configs["thermal_cycling_screening_strength"]["y_bins"] = args.temp_rate_bins
-	
+	# plots per fixed values of the screening strength
 	plots_per_ss = [
 		"thermal_cycling_number_cycles",
 		"thermal_cycling_test_time",
@@ -75,9 +81,47 @@ if __name__ == "__main__":
 		plot_config["x_bins"] = args.temp_range_bins
 		plot_config["y_bins"] = args.temp_rate_bins
 	
+	# plots per fixed values of the temperature range
+	plots_per_temp_range = [
+		"thermal_cycling_screening_strength",
+	]
+	plot_configs_per_temp_range = {}
+	for plot in plots_per_temp_range:
+		for temp_range, temp_range_string in zip(args.temp_ranges, temp_range_strings):
+			
+			plot_configs_per_temp_range.setdefault(plot, {})[temp_range_string] = jsonTools.JsonDict(os.path.join("plots", plot+".json"))
+			plot_configs_per_temp_range[plot][temp_range_string]["x_expressions"] = [x.replace("100.0", str(temp_range)) for x in plot_configs_per_temp_range[plot][temp_range_string]["x_expressions"]]
+			plot_configs_per_temp_range[plot][temp_range_string]["title"] = plot_configs_per_temp_range[plot][temp_range_string]["title"].replace("100K", str(temp_range)+"K")
+			plot_configs_per_temp_range[plot][temp_range_string]["output_dir"] = os.path.join(args.output_dir, "temp_range_"+temp_range_string)
+	
+	for plot_config in plot_configs_per_temp_range["thermal_cycling_screening_strength"].values():
+		plot_config["x_bins"] = args.n_cycles_bins
+		plot_config["y_bins"] = args.temp_rate_bins
+	
+	# plots per fixed values of the temperature rate of change
+	plots_per_temp_rate = [
+		"thermal_cycling_screening_strength_over_time_range",
+	]
+	plot_configs_per_temp_rate = {}
+	for plot in plots_per_temp_rate:
+		for temp_rate, temp_rate_string in zip(args.temp_rates, temp_rate_strings):
+			
+			plot_configs_per_temp_rate.setdefault(plot, {})[temp_rate_string] = jsonTools.JsonDict(os.path.join("plots", plot+".json"))
+			plot_configs_per_temp_rate[plot][temp_rate_string]["x_expressions"] = [x.replace("4.0", str(temp_rate)) for x in plot_configs_per_temp_rate[plot][temp_rate_string]["x_expressions"]]
+			plot_configs_per_temp_rate[plot][temp_rate_string]["title"] = plot_configs_per_temp_rate[plot][temp_rate_string]["title"].replace("4 K min", str(temp_rate)+" K min")
+			plot_configs_per_temp_rate[plot][temp_rate_string]["output_dir"] = os.path.join(args.output_dir, "temp_rate_"+temp_rate_string)
+	
+	for plot_config in plot_configs_per_temp_rate["thermal_cycling_screening_strength_over_time_range"].values():
+		plot_config["x_bins"] = args.test_time_bins
+		plot_config["y_bins"] = args.temp_range_bins
+	
 	list_of_config_dicts = plot_configs.values()
 	for tmp_plot_configs_per_ss in plot_configs_per_ss.values():
 		list_of_config_dicts.extend(tmp_plot_configs_per_ss.values())
+	for tmp_plot_configs_per_temp_range in plot_configs_per_temp_range.values():
+		list_of_config_dicts.extend(tmp_plot_configs_per_temp_range.values())
+	for tmp_plot_configs_per_temp_rate in plot_configs_per_temp_rate.values():
+		list_of_config_dicts.extend(tmp_plot_configs_per_temp_rate.values())
 	
 	if log.isEnabledFor(logging.DEBUG):
 		import pprint
