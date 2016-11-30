823e6a97917d95cca48a2834407ae0654d69f94c
JochiSt <jochen.steinmann@web.de>
2016-11-30 09:13:12 +0100

    turned comments into english

--- test/SQlite/sqlite.cpp
+++ test/SQlite/sqlite.cpp
@@ -8,36 +8,23 @@
 using namespace std;
  
 int main(){
-	// Als erstes legen wir einen sqlite3-Zeiger an
+	// create database pointer
 	sqlite3* Database;
-	// und instantiieren ein Objekt von string auf dem Stack
 	string Path;
 
-	// Danach fragen wir den Benutzer nach dem Pfad zur Datenbankdatei
-	cout << "Pfad zur Datenbankdatei: ";
+	cout << "Please enter path to database file ";
 	cin >> Path;
 
-	// Nun können wir die Datenbank öffnen!
-	if(sqlite3_open(Path.c_str(), &Database) != SQLITE_OK)
-	{
-		// Es ist ein Fehler aufgetreten!
-		cout << "Fehler beim Öffnen: " << sqlite3_errmsg(Database) << endl;
-
-		// Programm beenden
+	// open database
+	if(sqlite3_open(Path.c_str(), &Database) != SQLITE_OK){
+		cout << "Error while opening " << sqlite3_errmsg(Database) << endl;
+		// exit program
 		return 1;
 	}
-
-	// Es hat alles geklappt, die Datenbank wurde geöffnet
-	cout << "Datenbank erfolgreich geöffnet!" << endl;
 
 	
 
 
-	// Jetzt können wir die Datenbank wieder schließen
 	sqlite3_close(Database);
-
-	cout << "Datenbank geschlossen!" << endl;
-
-	// Programm beenden
 	return 0;   
 }
