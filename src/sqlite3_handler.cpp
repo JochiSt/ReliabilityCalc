5f51f0268c7b79f6783e2a0e6938dba9ceebd18b
JochiSt <jochen.steinmann@web.de>
2016-11-30 10:58:40 +0100

    removed unneeded comment

--- src/sqlite3_handler.cpp
+++ src/sqlite3_handler.cpp
@@ -4,7 +4,6 @@
 
 sqlite3_handler::sqlite3_handler(std::string filename){
 	/* Open database */
-	//int ret = sqlite3_open(filename.c_str(), db);
 	int ret = sqlite3_open_v2(filename.c_str(), &db, SQLITE_OPEN_READONLY, NULL);
 	if( ret ){
 		fprintf(stderr, "Can't open database: %s\n\t\t%s\n", filename.c_str(), sqlite3_errmsg(db));
