d6c105d1edcf07ddc7973cb770d6c5365c2831a1
JochiSt <jochen.steinmann@web.de>
2016-12-01 17:31:02 +0100

    changed import tool to output sql file, which can be imported. Somehow the sqlite3 interface is not working with import yet

--- tools/import_wuerth/import_wuerth.cpp
+++ tools/import_wuerth/import_wuerth.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #include <string.h>
 #include "sqlite3_handler.h"
 
-int main(){
+int main(int argc, char* argv[]){
     cout << "Import WUERTH reliability data" << endl;
     cout << "\tcompiled @ " << __DATE__ << " " << __TIME__ << " using GCC " << __VERSION__ << endl << endl;
 
@@ -24,27 +24,59 @@ int main(){
 
     printf("Will fill %3.1f %c\n", table, curve);
 
-    sqlite3_handler db("../../wuerth.db");
+//    sqlite3_handler db("../../wuerth.db");
 
     std::string filename;
-    cout << "Insert filename of table: " << endl;
-    cin >> filename;
+
+    if(argc > 1){
+	filename = std::string(argv[1]);
+    }else{
+        cout << "Insert filename of table: " << endl;
+        cin >> filename;
+    }
 
     ifstream in;
     in.open(filename.c_str());
 
+    std::string line;
+    getline(in, line);
+    std::cout << line << std::endl;
+
     float temperature, value;
     char query[1024];
+
+    ofstream out;
+    char filen[2014];
+    sprintf(filen, "%3.1f_%c.sql", table, curve);
+    out.open(filen);
+
+    int column;
+    cout << "Which column to use: (temperature = 0): " << endl;
+    cin >> column;
+
+    float value1, value2, value3, value4;
+
     while(true){
-        in >> temperature >> value;
+	char buffer[4096];
+	getline(in, line);
+	sprintf(buffer, "%s", line.c_str());
+	sscanf(buffer, "%f %f %f %f %f", &temperature, &value1, &value2, &value3, &value4);
+//        in >> temperature >> value;
+
+	if(column == 1) value = value1;
+	else if(column == 2) value = value2;
+	else if(column == 3) value = value3;
+	else if(column == 4) value = value4;
+
         if(!in.good()) break;
-        sprintf(query, "INSERT INTO reliability_data ('table', 'curve', 'temperature', 'FIT') VALUES ('%3.1f', '%c', '%f', '%f')", table, curve, temperature, value);
+        // INSERT INTO reliability_data VALUES (5.1, 'A', 0.003000, 0.751250, date('now') );
+        sprintf(query, "INSERT INTO reliability_data VALUES (\"%3.1f\", \"%c\", \"%f\", \"%f\", date('now'));", table, curve, temperature, value);
         printf("%s\n", query);
 
-        db.runSQL(std::string(query));
+        out << std::string(query) << endl;
+
+//        db.runSQL(std::string(query));
     }
-    // INSERT INTO reliability_data ('table', 'curve', 'temperature', 'FIT') VALUES ('5.7', 'f', '40', '0.05')
-
-
+    out.close();
     return 0;
 }
