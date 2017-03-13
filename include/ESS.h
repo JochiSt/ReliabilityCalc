22fd32ddd0ed70c5a6bcff252f944f5cf44511e9
JochiSt <jochen.steinmann@web.de>
2017-03-13 11:14:26 +0000

    started added class for calculations regarding Environmental Stress Screening

---
+++ include/ESS.h
@@ -0,0 +1,12 @@
+#ifndef ESS_H
+#define ESS_H
+
+class ESS {
+
+    public:
+	static float ScreenStrength(float Trange, float dT, int cycles);
+
+
+};
+
+#endif
---
+++ main_ESS.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ESS.h"
+
+
+int main(){
+
+
+    printf("ESS %f\n", ESS::ScreenStrength(80, 10, 2));
+
+   
+    return 0; 
+}
