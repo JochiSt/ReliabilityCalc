643e310cba096a3078523d3510d258759c1e3504
JochiSt <jochen.steinmann@web.de>
2017-06-20 22:29:39 +0100

    WÜRTH now offers capacitors with rated with more than 50V, so removed the limit

--- src/capacitor_WUERTH.cpp
+++ src/capacitor_WUERTH.cpp
@@ -4,8 +4,10 @@
 #include <float.h>
 
 capacitor_WUERTH::capacitor_WUERTH(std::string name, std::string code, float value, float usedU, float ratedU) : capacitor(name, value, usedU, ratedU), component_WUERTH(code){
-    if(ratedVoltage > 50)   // do check, whether rated voltage is ok
-        ratedVoltage = 50;
+    if(code == "WCAP-GSGP" ) {
+	    if(ratedVoltage > 50)   // do check, whether rated voltage is ok
+		ratedVoltage = 50;
+    }
 }
 capacitor_WUERTH::capacitor_WUERTH(std::string name, std::string code, float value, float stress) : capacitor(name, value, stress), component_WUERTH(code){
 
