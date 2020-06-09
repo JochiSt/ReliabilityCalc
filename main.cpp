caef50df3f42c6b2cf62676be63a8fe32c38a3f2
Jochen Steinmann <jochen.steinmann@rwth-aachen.de>
2020-06-09 06:11:48 +0200

    changed to a more minimal example

--- main.cpp
+++ main.cpp
@@ -36,24 +36,15 @@ int main(){
 
     schematic* example = new schematic("Example Board");
 
-    example -> addComponent(new capacitor_WUERTH("C1", "WCAP-CSGP",  1*capacitor::uF,  1, 50)); // lowest stress
-    example -> addComponent(new capacitor_WUERTH("C2", "WCAP-CSGP",  1*capacitor::uF, 14, 50));
-    example -> addComponent(new capacitor_WUERTH("C2a", "WCAP-CSGP",  1*capacitor::uF, 14, 50), 10);
-    example -> addComponent(new capacitor_WUERTH("C3", "WCAP-CSGP",  1*capacitor::uF, 24, 50));
-    example -> addComponent(new capacitor_WUERTH("C4", "WCAP-CSGP",  1*capacitor::uF, 34, 50));
-    example -> addComponent(new capacitor_WUERTH("C5", "WCAP-CSGP",  1*capacitor::uF, 50, 50)); // maximal stress
-
     example -> addComponent(new capacitor_VISHAY("C90", capacitor_VISHAY::CLASS_1, 1*capacitor::pF, 24, 50));
     example -> addComponent(new capacitor_VISHAY("C91", capacitor_VISHAY::CLASS_2, 1*capacitor::pF, 24, 50));
 
     example -> addComponent(new capacitor_WIMA("C10", 1*capacitor::uF, 24, 50));
     example -> addComponent(new capacitor_WIMA_SMD_PPS("C10", 1*capacitor::uF, 24, 50));
 
-    example -> addComponent(new inductor_WUERTH("L1", "WE-PoE+"));
-
     example -> addComponent(new crystal("Q1", 36, 25));
     example -> addComponent(new crystal_EPSON("Q1", 36, 25));
-
+/*
     example -> addComponent(new IC_TI("GCU_ISO", "ISO7341CQDWQ1"));
     example -> addComponent(new IC_TI("GCU_ISO", "DS30EA101"));
     example -> getLastComponent() -> setDeviceTemperature(55);
@@ -62,7 +53,7 @@ int main(){
     example -> addComponent(new IC_MAXIM("IC2", "MAX485"));
 
     example -> addComponent(new IC_AD("IC5", "ADP1765"));
-
+*/
     example -> addComponent(new PCB("PCB", 4 ,1000, PCB::MICRO_VIA , 500)); 
 
     example -> setVerboseOutput(true);  // enable verbose output
