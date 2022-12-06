12d2b3201e9b7dc8b5555ca105a2cde7f3bc50c8
JochiSt <jochen.steinmann@web.de>
2022-12-06 18:34:34 +0100

    added part counter variable
    and included stress for resistors

--- ReliabilityCalc/components/ic/ic.py
+++ ReliabilityCalc/components/ic/ic.py
@@ -4,8 +4,17 @@ sys.path.append("../../")
 from components import component
 
 class ic(component):
+
+    partcnt = 0
+
+    @staticmethod
+    def getPartCnt():
+        return __class__.partcnt
+
     def __init__(self, value, T=None, deltaT=None):
         super().__init__(T, deltaT)
         assert value
         self.value = value
+
+        __class__.partcnt += 1
 
--- ReliabilityCalc/components/resistor/resistor.py
+++ ReliabilityCalc/components/resistor/resistor.py
@@ -7,7 +7,24 @@ sys.path.append("../../")
 from components import component
 
 class resistor(component):
+
+    partcnt = 0;
+
     def __init__(self, value, T=None, deltaT=None):
         super().__init__(T, deltaT)
         assert value
         self.value = value
+
+        __class__.partcnt += 1
+
+        self.stress = 0
+
+    def setStress(self, stress):
+        self.stress = stress
+
+    def setStress(self, ratedPower, usedPower):
+        self.stress = usedPower / ratedPower
+
+    @staticmethod
+    def getPartCnt():
+        return __class__.partcnt
