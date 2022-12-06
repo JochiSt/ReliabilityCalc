5b680504763f34347159e0c4181ee979e0f42ccd
JochiSt <jochen.steinmann@web.de>
2022-12-06 12:05:57 +0100

    ensure, that a value is always present

--- ReliabilityCalc/components/inductor/inductor.py
+++ ReliabilityCalc/components/inductor/inductor.py
@@ -6,5 +6,6 @@ from components import component
 class inductor(component):
     def __init__(self, value, T=None, deltaT=None):
         super().__init__(T, deltaT)
+        assert value
         self.value = value
 
