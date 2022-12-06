03a40a7c09ce2c13aa9ca25748d9527bbd34eb79
JochiSt <jochen.steinmann@web.de>
2022-12-06 10:10:20 +0100

    include setting of global Temperature and local Temperature

--- ReliabilityCalc/components/component.py
+++ ReliabilityCalc/components/component.py
@@ -1,9 +1,31 @@
 # -*- coding: utf-8 -*-
 
 class component:
+
+    T = 293 # global temperature
+
     def __init__(self, T):
         print("init with temperature " + str(T) )
-        self.T = T
+        if T is None:
+            self.useGlobalT = True
+        else:
+            self.useGlobalT = False
+            self.T = T
 
     def FIT(self):
-        return self.T
+        return -1
+
+    @staticmethod
+    def setGlobalTemperature(T):
+        __class__.T = T
+
+    def getGlobalTemperature(T):
+        return __class__.T
+
+    def getTemperature(self):
+        if self.useGlobalT:
+            return __class__.T
+        else:
+            return self.T
+
+
